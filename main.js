f0081b70bc29c8db4e62812560d2ceaffc9ed8a6
sabatali <126473010+sabatali@users.noreply.github.com>
2023-07-10 03:01:51 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
+$('.dropdown').click(function(e){
+  e.stopPropagation();
+  $('.nav-dropdown').toggle();
+});
 
+$('html').click(function(){
+  $('.nav-dropdown').hide();
+})
 
+$('#nav-toggle').on('click', function() {
+  this.classList.toggle('active');
+});
 
-(function() {
-  if (document.querySelector("#top-nav-toggle")) {
-    var navToggle = document.querySelector("#top-nav-toggle");
-
-    function watchNavClose(e) {
-      var topNav = document.querySelector(".top-bar");
-      if (!e.path.includes(topNav)) {
-        openCloseNav();
-        document.documentElement.removeEventListener("click", watchNavClose);
-      }
-    }
-
-    function openCloseNav() {
-      var navToggle = document.querySelector("#top-nav-toggle");
-
-      if (!navToggle.classList.contains("closed")) {
-        navToggle.classList.add("closed");
-        document.querySelector("#top-bar__nav").classList.remove("collapsed");
-        document.querySelector("html").addEventListener("click", watchNavClose);
-      } else {
-        navToggle.classList.remove("closed");
-        document.querySelector("#top-bar__nav").classList.add("collapsed");
-        document.documentElement.removeEventListener("click", watchNavClose);
-      }
-    }
-
-    navToggle.addEventListener("click", openCloseNav);
-  }
-})();
-
+$("#nav-toggle").click(function() {
+      $("nav ul").toggle();
+    });
+    
+    
